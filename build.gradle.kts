b85df933a15315828aa81dcd3111d39289563ffc
shairo009 <shairo009@users.noreply.github.com>
2026-06-16 03:50:36 +0530

    Fix repository configuration conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,12 +10,6 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
